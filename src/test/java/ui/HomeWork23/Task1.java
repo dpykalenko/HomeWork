6eb1b30298d38b5bbe14115c315e876ef943a8de
Diana Pykalenko <dianapykalenko@gmail.com>
2021-05-30 22:41:05 +0300

    Home Work 23 (add 1 task)

--- src/test/java/ui/HomeWork23/Task1.java
+++ src/test/java/ui/HomeWork23/Task1.java
@@ -15,11 +15,14 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class Task1 {
     String initialUrl = "https://rozetka.com.ua/";
+    By uploadedElements = By.xpath("//div[@class='goods-tile__inner']");
     WebDriver driver;
     WebDriverWait wait;
+    final int maxPrice = 4000;
 
     @BeforeMethod
     public void navigateAction(){
@@ -46,22 +49,89 @@ public class Task1 {
         builder.moveToElement(laptopsLink).perform();
         wait.until(presenceOfElementLocated(monitorXpath));
         builder.moveToElement(driver.findElement(monitorXpath)).click().perform();
-        wait.until(presenceOfElementLocated(By.xpath("//div[@class='goods-tile__inner']")));
+
+        FilterMonitorsAndSelect(maxPrice);
+        wait.until(presenceOfElementLocated(By.xpath("//div[@class='product-about__right']")));
+        driver.findElement(By.xpath("//*[@class='compare-button ng-star-inserted']")).click();
+        wait.until(presenceOfElementLocated(By.xpath("//rz-icon-counter//span")));
+        assertEquals(driver.findElement(By.xpath("//rz-icon-counter//span")).getText(), "1");
+
+        int priceOfFirstMonitor1 = getMonitorPriceOnSelectionPage();
+        String nameOfFirstMonitor1 = getMonitorName();
+
+        driver.navigate().back();
+
+        FilterMonitorsAndSelect(priceOfFirstMonitor1);
+        driver.findElement(By.xpath("//*[@class='compare-button ng-star-inserted']")).click();
+        wait.until(presenceOfElementLocated(By.xpath("//rz-icon-counter//span")));
+        assertEquals(driver.findElement(By.xpath("//rz-icon-counter//span")).getText(), "2");
+
+        int priceOfFirstMonitor2 = getMonitorPriceOnSelectionPage();
+        String nameOfFirstMonitor2 = getMonitorName();
+
+        driver.findElement(By.xpath("//rz-comparison")).click();
+        wait.until(presenceOfElementLocated(By.xpath("//a[@class='comparison-modal__link']")));
+        driver.findElement(By.xpath("//a[@class='comparison-modal__link']")).click();
+
+        List<WebElement> compareBlocks = driver.findElements(By.xpath("//li[@class='products-grid__cell ng-star-inserted']"));
+        wait.until(presenceOfElementLocated(By.xpath("//ul[@class='products-grid']")));
+        assertEquals(compareBlocks.size(), 2);
+
+        WebElement monitorElement1 = driver.findElement(By.xpath("(//a[@class='product__heading'])[2]"));
+        String actualNameOfMonitor1 = monitorElement1.getText();
+        assertEquals(actualNameOfMonitor1, nameOfFirstMonitor1);
+
+        WebElement monitorElement2 = driver.findElement(By.xpath("(//a[@class='product__heading'])[1]"));
+        String actualNameOfMonitor2 = monitorElement2.getText();
+        assertEquals(actualNameOfMonitor2, nameOfFirstMonitor2);
+
+        int actualpriceOfMonitor1 = getMonitorPriceOnComparePage(2);
+        assertEquals(actualpriceOfMonitor1, priceOfFirstMonitor1);
+
+        int actualpriceOfMonitor2 = getMonitorPriceOnComparePage(1);
+        assertEquals(actualpriceOfMonitor2, priceOfFirstMonitor2);
+
+    }
+
+    public int getMonitorPriceOnSelectionPage(){
+        WebElement priceElement = driver.findElement(By.xpath("//*[@class='product-prices__big product-prices__big_color_red']"));
+        String priceText = priceElement.getText();
+        String priceTextClear = priceText
+                .replaceAll("\\s", "")
+                .replace("₴","");
+        return Integer.parseInt(priceTextClear);
+    }
+
+    public int getMonitorPriceOnComparePage(int monitorOrder){
+        String priceText = driver.findElement(By.xpath("(//div[@class='product__price product__price--red ng-star-inserted'])[" + monitorOrder + "]")).getText();
+        boolean hasNewPrice = priceText.contains("\n");
+        if (hasNewPrice){
+            priceText = priceText.split("\n")[1];
+        }
+        String priceTextClear = priceText
+                .replaceAll("\\s", "")
+                .replace("₴","");
+        return Integer.parseInt(priceTextClear);
+    }
+
+    public String getMonitorName(){
+        return driver.findElement(By.xpath("//*[@class='product__title']")).getText();
+    }
+
+    public void FilterMonitorsAndSelect(int priceLimit){
+        wait.until(presenceOfElementLocated(uploadedElements));
         List<WebElement> resultTiles = driver.findElements(By.xpath("//div[@class='goods-tile__inner']"));
         int i = 1;
         for (WebElement tile : resultTiles) {
             WebElement span = tile.findElement(By.xpath("(//span[@class='goods-tile__price-value'])[" + i + "]"));
             String priceText = span.getText().replaceAll("\\s", "");
             int price = Integer.parseInt(priceText);
-            if (price < 4000) {
-                WebElement image = tile.findElement(By.xpath("(//div[@class='goods-tile__inner'])[" + i + "]/a[@class='goods-tile__picture ng-star-inserted']/img[1]"));
+            if (price < priceLimit) {
+                WebElement image = tile.findElement(By.xpath("//li[@class='catalog-grid__cell catalog-grid__cell_type_slim ng-star-inserted'][" + i + "]//span[@class='goods-tile__title']"));
                 image.click();
                 break;
             }
             i++;
-
-
         }
     }
-
 }
